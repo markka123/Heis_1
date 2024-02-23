06ea366fd505d70d3c739874eae478f5609fd093
arneskog <arneskog@stud.ntnu.no>
2024-02-23 14:38:21 +0100

    added functionality to take order, and move up/down/stop

--- lab_2-0.1.0/skeleton_project/source/ownSource/createOrder.c
+++ lab_2-0.1.0/skeleton_project/source/ownSource/createOrder.c
@@ -4,22 +4,22 @@
 Order createOrder(int floor, int btn) {
     Order o;
     o.floor = floor;
-    o.btn = whichButton(btn);
+    o.btn = (whichButton)btn;
     return o;
 }
 
 
 //mulig å bruke
-Order orderCreated() {
-    for(int f = 0; f < N_FLOORS; f++){
-            for(int b = 0; b < N_BUTTONS; b++){
-                int btnPressed = elevio_callButton(f, b);
-                if(btnPressed != 0) {
-                    return createOrder(f, b); //returns an order
-                }
+// Order orderCreated() {
+//     for(int f = 0; f < N_FLOORS; f++){
+//             for(int b = 0; b < N_BUTTONS; b++){
+//                 int btnPressed = elevio_callButton(f, b);
+//                 if(btnPressed != 0) {
+//                     return createOrder(f, b); //returns an order
+//                 }
 
-            }
-        }
-}
+//             }
+//         }
+// }
 
 
